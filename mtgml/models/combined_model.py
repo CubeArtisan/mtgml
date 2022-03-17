ffd0f7d862381781f3043f394bfd2d054aa1119f
Devon Richards <friends.devon@gmail.com>
2022-03-17 12:12:42 -0500

    chore(paper): Ignore intermediate products of the paper.

--- mtgml/models/combined_model.py
+++ mtgml/models/combined_model.py
@@ -12,24 +12,29 @@ class CombinedModel(ConfigurableLayer, tf.keras.models.Model):
     def get_properties(cls, hyper_config, input_shapes=None):
         num_cards = hyper_config.get_int('num_cards', min=1, max=None, default=None,
                                          help='The number of items that must be embedded. Should be 1 + the max index expected to see.')
-        embed_cards = hyper_config.get_sublayer('EmbedCards', sub_layer_type=ItemEmbedding,
+        result = {
+            'embed_cards': hyper_config.get_sublayer('EmbedCards', sub_layer_type=ItemEmbedding,
                                              fixed={'num_items': num_cards},
-                                             seed_mod=29, help='The embeddings for the card objects.')
-        return {
-            'embed_cards': embed_cards,
+                                             seed_mod=29, help='The embeddings for the card objects.'),
             'draftbots': hyper_config.get_sublayer('DraftBots', sub_layer_type=DraftBot,
                                                    fixed={'num_cards': num_cards}, seed_mod=13,
                                                    help='The model for the draftbots'),
-            # 'cube_recommender': hyper_config.get_sublayer('CubeRecommender', sub_layer_type=CubeRecommender,
-            #                                               fixed={'num_cards': num_cards}, seed_mod=17,
-            #                                               help='The model for the draftbots'),
-            # 'cube_adj_mtx_reconstructor': hyper_config.get_sublayer('CubeAdjMtxReconstructor', sub_layer_type=AdjMtxReconstructor,
-            #                                                         fixed={'num_cards': num_cards}, seed_mod=19,
-            #                                                         help='The model to reconstruct the cube adjacency matrix'),
-            # 'deck_adj_mtx_reconstructor': hyper_config.get_sublayer('DeckAdjMtxReconstructor', sub_layer_type=AdjMtxReconstructor,
-            #                                                         fixed={'num_cards': num_cards}, seed_mod=23,
-            #                                                         help='The model to reconstruct the deck adjacency matrix')
+            'just_draftbots': hyper_config.get_bool('just_draftbots', default=True,
+                                                    help="Whether to just train the draftbots."),
         }
+        if not result['just_draftbots']:
+            result |= {
+                'cube_recommender': hyper_config.get_sublayer('CubeRecommender', sub_layer_type=CubeRecommender,
+                                                              fixed={'num_cards': num_cards}, seed_mod=17,
+                                                              help='The model for the draftbots'),
+                'cube_adj_mtx_reconstructor': hyper_config.get_sublayer('CubeAdjMtxReconstructor', sub_layer_type=AdjMtxReconstructor,
+                                                                        fixed={'num_cards': num_cards}, seed_mod=19,
+                                                                        help='The model to reconstruct the cube adjacency matrix'),
+                'deck_adj_mtx_reconstructor': hyper_config.get_sublayer('DeckAdjMtxReconstructor', sub_layer_type=AdjMtxReconstructor,
+                                                                        fixed={'num_cards': num_cards}, seed_mod=23,
+                                                                        help='The model to reconstruct the deck adjacency matrix')
+            }
+        return result
 
     def build(self, input_shapes):
         super(CombinedModel, self).build(input_shapes)
@@ -41,11 +46,14 @@ class CombinedModel(ConfigurableLayer, tf.keras.models.Model):
                 draftbot_loss = self.draftbots((*inputs[0][0:-2], self.embed_cards.embeddings, *inputs[0][-2:]), training=training)
             else:
                 draftbot_loss = self.draftbots((*inputs[0], self.embed_cards.embeddings), training=training)
-            # recommender_loss = self.cube_recommender((*inputs[1], self.embed_cards.embeddings), training=training)
-            # cube_adj_mtx_loss = self.cube_adj_mtx_reconstructor((*inputs[2], self.embed_cards.embeddings), training=training)
-            # deck_adj_mtx_loss = self.deck_adj_mtx_reconstructor((*inputs[2], self.embed_cards.embeddings), training=training)
+            if not self.just_draftbots:
+                recommender_loss = self.cube_recommender((*inputs[1], self.embed_cards.embeddings), training=training)
+                cube_adj_mtx_loss = self.cube_adj_mtx_reconstructor((*inputs[2], self.embed_cards.embeddings), training=training)
+                deck_adj_mtx_loss = self.deck_adj_mtx_reconstructor((*inputs[2], self.embed_cards.embeddings), training=training)
         if len(inputs[0]) > 9:
-            self.add_loss(draftbot_loss)
-            tf.summary.scalar('loss', draftbot_loss)
-            # self.add_loss(draftbot_loss + recommender_loss + cube_adj_mtx_loss + deck_adj_mtx_loss)
+            loss = draftbot_loss
+            if not self.just_drafbots:
+                loss += recommender_loss + cube_adj_mtx_loss + deck_adj_mtx_loss
+            self.add_loss(loss)
+            tf.summary.scalar('loss', loss)
         return ()
