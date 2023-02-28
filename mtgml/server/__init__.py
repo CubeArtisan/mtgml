bd78acb42566bf4a46b5799a9d632b8b0a57b223
Devon Richards <friends.devon@gmail.com>
2023-02-28 14:18:58 -0600

    shrink model size.

--- mtgml/server/__init__.py
+++ mtgml/server/__init__.py
@@ -12,17 +12,16 @@ from opentelemetry import trace
 from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.propagate import set_global_textmap
-from opentelemetry.propagators.cloud_trace_propagator import (
-    CloudTraceFormatPropagator,
+from opentelemetry.propagators.cloud_trace_propagator import CloudTraceFormatPropagator
+from opentelemetry.resourcedetector.gcp_resource_detector import (
+    GoogleCloudResourceDetector,
 )
 from opentelemetry.sdk.resources import get_aggregated_resources
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
+from opentelemetry.sdk.trace.export import (  # SimpleExportSpanProcessor,
     BatchSpanProcessor,
     ConsoleSpanExporter,
-    # SimpleExportSpanProcessor,
 )
-from opentelemetry.resourcedetector.gcp_resource_detector import GoogleCloudResourceDetector
 
 from mtgml.server.cube_recommender import get_cube_recomendations
 from mtgml.server.draftbots import get_draft_scores
@@ -31,9 +30,7 @@ Flask = flask.Flask
 request = flask.request
 
 try:
-    resources = get_aggregated_resources(
-        [GoogleCloudResourceDetector(raise_on_error=True)]
-    )
+    resources = get_aggregated_resources([GoogleCloudResourceDetector(raise_on_error=True)])
 
     # set_global_textmap(CloudTraceFormatPropagator())
 
@@ -48,7 +45,7 @@ try:
 except Exception:
     tracer_provider = TracerProvider()
     # tracer_provider.add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
-    logging.error('Failed to initialize cloud tracing')
+    logging.error("Failed to initialize cloud tracing")
 
 trace.set_tracer_provider(tracer_provider)
 tracer = trace.get_tracer(__name__)
@@ -63,46 +60,46 @@ def stdout_log_filter(record):
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setLevel(logging.INFO)
 stdout_handler.addFilter(stdout_log_filter)
-stdout_formatter = logging.Formatter('%(asctime)s - mtgml-server-stdout - %(levelname)s - %(message)s')
+stdout_formatter = logging.Formatter("%(asctime)s - mtgml-server-stdout - %(levelname)s - %(message)s")
 stdout_handler.setFormatter(stdout_formatter)
 stderr_handler = logging.StreamHandler(sys.stderr)
 stderr_handler.setLevel(logging.WARNING)
-stderr_formatter = logging.Formatter('%(asctime)s - mtgml-server-stderr - %(levelname)s - %(message)s')
+stderr_formatter = logging.Formatter("%(asctime)s - mtgml-server-stderr - %(levelname)s - %(message)s")
 stderr_handler.setFormatter(stderr_formatter)
 
 logger = app.logger
 logger.propagate = False
 logger.handlers.extend((stdout_handler, stderr_handler))
 logger.setLevel(logging.INFO)
-logger.info('Starting up the server')
+logger.info("Starting up the server")
 
 try:
     FlaskInstrumentor().instrument_app(app)
 except Exception:
-    logger.error('Failed to attach instrumenter to flask app.')
+    logger.error("Failed to attach instrumenter to flask app.")
 
-MODEL_PATH = Path('ml_files/testing_tflite')
+MODEL_PATH = Path("ml_files/testing_tflite")
 MODEL = None
-with open(MODEL_PATH / 'int_to_oracle_id.json', 'r') as map_file:
+with open(MODEL_PATH / "int_to_oracle_id.json", "r") as map_file:
     int_to_card = json.load(map_file)
-original_to_new_path = MODEL_PATH / 'original_to_new_index.json'
+original_to_new_path = MODEL_PATH / "original_to_new_index.json"
 if original_to_new_path.exists():
-    with original_to_new_path.open('r') as fp:
+    with original_to_new_path.open("r") as fp:
         original_to_new_index = json.load(fp)
 else:
     original_to_new_index = tuple(range(len(int_to_card) + 1))
 CARD_TO_INT = {v: k for k, v in enumerate(int_to_card)}
 INT_TO_CARD = int_to_card
-AUTH_TOKENS = os.environ.get('MTGML_AUTH_TOKENS', 'testing').split(';')
-AUTH_ENABLED = os.environ.get('MTGML_AUTH_ENABLED', 'False') == 'True'
+AUTH_TOKENS = os.environ.get("MTGML_AUTH_TOKENS", "testing").split(";")
+AUTH_ENABLED = os.environ.get("MTGML_AUTH_ENABLED", "False") == "True"
 
 
 @app.before_request
 def verify_request():
-    if not AUTH_ENABLED:
+    if not AUTH_ENABLED or "/version" in request.path:
         return None
     else:
-        logger.warn(os.environ.get('MTGML_AUTH_ENABLED'))
+        logger.warn(os.environ.get("MTGML_AUTH_ENABLED"))
         token = request.args.get("auth_token", None)
         if token is None or token not in AUTH_TOKENS:
             return {"success": False, "error": "Not authorized"}, 401
@@ -113,13 +110,13 @@ def verify_request():
 def get_model():
     global MODEL
     if MODEL is None:
-        MODEL = tflite.Interpreter(model_path=str(MODEL_PATH / 'combined_model.tflite'))
+        MODEL = tflite.Interpreter(model_path=str(MODEL_PATH / "combined_model.tflite"))
         MODEL.allocate_tensors()
     return MODEL
 
 
 # This is a POST request since it needs to take in a cube object
-@app.route("/cube", methods=['POST'])
+@app.route("/cube", methods=["POST"])
 def cube_recommendations():
     num_recs = int(request.args.get("num_recs", 100))
     json_data = request.get_json()
@@ -129,13 +126,20 @@ def cube_recommendations():
         return {"success": False, "error": error}, 400
     cube = json_data.get("cube")
     model = get_model()
-    results = get_cube_recomendations(cube=cube, model=model, card_to_int=CARD_TO_INT, int_to_card=INT_TO_CARD,
-                                      tracer=tracer, num_recs=num_recs, original_to_new_index=original_to_new_index)
+    results = get_cube_recomendations(
+        cube=cube,
+        model=model,
+        card_to_int=CARD_TO_INT,
+        int_to_card=INT_TO_CARD,
+        tracer=tracer,
+        num_recs=num_recs,
+        original_to_new_index=original_to_new_index,
+    )
     return results, 200
 
 
 # This is a POST request since it needs to take in a drafter state object
-@app.route("/draft", methods=['POST'])
+@app.route("/draft", methods=["POST"])
 def draft_recommendations():
     json_data = request.get_json()
     if json_data is None or "drafterState" not in json_data:
@@ -144,21 +148,22 @@ def draft_recommendations():
         return {"success": False, "error": error}, 400
     drafter_state = json_data.get("drafterState")
     model = get_model()
-    results = get_draft_scores(drafter_state, model=model, card_to_int=CARD_TO_INT, tracer=tracer,
-                               original_to_new_index=original_to_new_index)
+    results = get_draft_scores(
+        drafter_state, model=model, card_to_int=CARD_TO_INT, tracer=tracer, original_to_new_index=original_to_new_index
+    )
     return results, 200
 
 
-@app.route('/version', methods=['GET'])
+@app.route("/version", methods=["GET"])
 def get_version():
-    return {'version': os.environ.get('MTGML_VERSION', 'alpha'), 'success': True}, 200
+    return {"version": os.environ.get("MTGML_VERSION", "alpha"), "success": True}, 200
 
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
     return response
 
 
--- scripts/update_data.sh
+++ scripts/update_data.sh
@@ -5,17 +5,16 @@ set -e
 mkdir -p data/CubeCobra/drafts data/CubeCobra/decks data/CubeCobra/cubes data/maps
 cd data
 rm -rf maps/int_to_card.json maps/card_to_int.json
-#
-# if aws s3 ls s3://cubecobra/ > /dev/null; then
-#   mkdir -p CubeCobra
-#   aws s3 sync s3://cubecobra/draft_picks CubeCobra/drafts
-#   aws s3 sync s3://cubecobra/deck_exports CubeCobra/decks
-#   aws s3 sync s3://cubecobra/cube_exports CubeCobra/cubes
-#   parallel sed "'s/[$#]:\"/\":\"/g'" -i {} ::: CubeCobra/{decks,drafts}/*.json
-#   parallel sed "'s/\`/\"/g'" -i {} ::: CubeCobra/{decks,drafts}/*.json
-# else
-#   echo "You don't have access to the CubeCobra aws bucket. Talk to Dekkaru to get access." 1>2
-# fi
+
+if aws s3 ls s3://cubecobra/ > /dev/null; then
+  mkdir -p CubeCobra
+  # aws s3 sync s3://cubecobra/deck_exports CubeCobra/decks
+  aws s3 sync s3://cubecobra/cube_exports CubeCobra/cubes
+  # parallel sed "'s/[$#]:\"/\":\"/g'" -i {} ::: CubeCobra/{decks,drafts}/*.json
+  # parallel sed "'s/\`/\"/g'" -i {} ::: CubeCobra/{decks,drafts}/*.json
+else
+  echo "You don't have access to the CubeCobra aws bucket. Talk to Dekkaru to get access." 1>2
+fi
 
 export GS_PATH=gs://cubeartisan/exports/
 export DATE=`gsutil ls -lh $GS_PATH\
@@ -35,19 +34,15 @@ cp $DATE/int_to_card.json maps/int_to_card.json
 cp $DATE/card_to_int.json maps/card_to_int.json
 
 cd ..
-python -m mtgml.preprocessing.find_used data/$DATE/drafts\;data/$DATE/draftlogs nonexistant data/$DATE/cubes data/CubeCobra/cubes
-if [[ -d data/CubeCobra/decks ]]; then
-    python -m mtgml.preprocessing.load_decks data/$DATE/decks data/CubeCobra/decks
-else
-    python -m mtgml.preprocessing.load_decks data/$DATE/decks
-fi
+python -m mtgml.preprocessing.find_used data/$DATE/drafts\;data/$DATE/draftlogs data/$DATE/cubes data/CubeCobra/cubes
+# if [[ -d data/CubeCobra/decks ]]; then
+#     python -m mtgml.preprocessing.load_decks data/$DATE/decks data/CubeCobra/decks
+# else
+#     python -m mtgml.preprocessing.load_decks data/$DATE/decks
+# fi
 if [[ -d data/CubeCobra/cubes ]]; then
     python -m mtgml.preprocessing.load_cubes data/$DATE/cubes data/CubeCobra/cubes
 else
     python -m mtgml.preprocessing.load_cubes data/$DATE/cubes
 fi
-if [[ -d data/CubeCobra/drafts ]]; then
-    python -m mtgml.preprocessing.load_picks data/$DATE/drafts\;data/$DATE/draftlogs data/CubeCobra/drafts
-else
-    python -m mtgml.preprocessing.load_picks data/$DATE/drafts\;data/$DATE/draftlogs
-fi
+python -m mtgml.preprocessing.load_picks data/$DATE/drafts\;data/$DATE/draftlogs
