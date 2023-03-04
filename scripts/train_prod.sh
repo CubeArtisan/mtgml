3b54e78f44b562199d2c04b6cf1b90adfc33e442
Devon Richards <friends.devon@gmail.com>
2023-03-04 11:43:16 -0600

    Only train for two epochs to avoid overfitting.

--- scripts/train_prod.sh
+++ scripts/train_prod.sh
@@ -41,21 +41,4 @@ cp examples/prod.pre.yaml ml_files/train_prod/hyper_config.yaml
 python -m mtgml.training.train_combined --name train_prod --epochs 8 --seed 268459 || true
 cp ml_files/train_prod/hyper_config.yaml ml_files/train_prod/hyper_config.pre.yaml
 cp examples/prod.yaml ml_files/train_prod/hyper_config.yaml
-for i in {1..8}; do python -m mtgml.training.train_combined --name train_prod --epochs 16 --seed 16809$i$i || true; done
-
-rm -rf ml_files/latest
-mkdir ml_files/latest
-cp ml_files/train_prod/* ml_files/latest
-cp data/maps/int_to_card.json ml_files/latest
-cp data/maps/original_to_new_index.json ml_files/latest
-
-rm -r ml_files/testing_tflite
-python -m mtgml.postprocessing.patch_model
-
-export REPOSITORY=ghcr.io/cubeartisan
-export REPOSITORY2=gcr.io/cubeartisan
-
-docker-compose -f .docker/docker-compose.yml build
-docker-compose -f .docker/docker-compose.yml push
-docker tag $REPOSITORY/mtgml:$TAG $REPOSITORY2/mtgml:$TAG
-docker push $REPOSITORY2/mtgml:$TAG
+python -m mtgml.training.train_combined --name train_prod --epochs 2 --seed 16809
