b3dca7530e27dc85bc49bbfdfc7ad7cb291ce693
Devon Richards <friends.devon@gmail.com>
2023-08-09 10:32:48 -0500

    feat(sets): Add new sets to the automation scripts.

--- flake.nix
+++ flake.nix
@@ -12,17 +12,18 @@
           inherit system;
           config.allowUnfree = true;
         };
+        cudaPackages = pkgs.cudaPackages_11_8;
         pythonPackages = pkgs.python310Packages;
       in
         {
           devShell = pkgs.mkShell {
             name = "mtgml";
             buildInputs = [
-              pkgs.cudaPackages.cudatoolkit
-              pkgs.cudaPackages.cudnn
-              pkgs.cudaPackages.cutensor
-              pkgs.cudaPackages.libcublas
-              pkgs.cudaPackages.tensorrt
+              cudaPackages.cudatoolkit
+              cudaPackages.cudnn
+              cudaPackages.cutensor
+              cudaPackages.libcublas
+              cudaPackages.tensorrt
               pkgs.linuxPackages.nvidia_x11
               pkgs.poetry
               pythonPackages.python
@@ -39,29 +40,23 @@
             ];
 
             shellHook = ''
-              export CUDATOOLKIT=${pkgs.cudaPackages.cudatoolkit}
-              export CUDATOOLKIT_LIB=${pkgs.cudaPackages.cudatoolkit.lib}
-              export CUDA_DIR=$CUDATOOLKIT_LIB
-              export CUDNN=${pkgs.cudaPackages.cudnn}
-              export CUTENSOR=${pkgs.cudaPackages.cutensor}
-              export CUBLAS=${pkgs.cudaPackages.libcublas}
-              export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudatoolkit}/lib:${pkgs.cudaPackages.cudatoolkit.lib}/lib:$LD_LIBRARY_PATH
-              export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudnn}/lib:${pkgs.cudaPackages.cutensor}/lib:$LD_LIBRARY_PATH
-              export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudatoolkit}/nvvm/libdevice:$LD_LIBRARY_PATH
-              export LD_LIBRARY_PATH=${pkgs.cudaPackages.libcublas}/lib:$LD_LIBRARY_PATH
-              export LD_LIBRARY_PATH=${pkgs.cudaPackages.tensorrt}/lib:${pkgs.linuxPackages.nvidia_x11}/lib:$LD_LIBRARY_PATH
+              export LD_LIBRARY_PATH=${cudaPackages.cudatoolkit}/lib:${cudaPackages.cudatoolkit.lib}/lib:$LD_LIBRARY_PATH
+              export LD_LIBRARY_PATH=${cudaPackages.cudnn}/lib:${cudaPackages.cutensor}/lib:$LD_LIBRARY_PATH
+              export LD_LIBRARY_PATH=${cudaPackages.cudatoolkit}/nvvm/libdevice:$LD_LIBRARY_PATH
+              export LD_LIBRARY_PATH=${cudaPackages.libcublas}/lib:$LD_LIBRARY_PATH
+              export LD_LIBRARY_PATH=${cudaPackages.tensorrt}/lib:${pkgs.linuxPackages.nvidia_x11}/lib:$LD_LIBRARY_PATH
               export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:$LD_LIBRARY_PATH
               export LD_LIBRARY_PATH=${pkgs.yajl}/lib:$LD_LIBRARY_PATH
 
               # export TF_GPU_ALLOCATOR=cuda_malloc_async
-              export XLA_FLAGS="--xla_gpu_enable_fast_min_max --xla_gpu_cuda_data_dir=${pkgs.cudaPackages.cudatoolkit}"
+              export XLA_FLAGS="--xla_gpu_enable_fast_min_max --xla_gpu_cuda_data_dir=${cudaPackages.cudatoolkit}"
               export TF_XLA_FLAGS="--tf_xla_cpu_global_jit --tf_xla_enable_lazy_compilation  --tf_xla_async_compilation"
               export TF_GPU_THREAD_MODE=gpu_private
 
               poetry env use 3.10
+              poetry install --sync
               export VENV_DIR=$(poetry env info --path)
               source $VENV_DIR/bin/activate
-              poetry install --sync
               python setup.py install --build-type Release
             '';
           };
